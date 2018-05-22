ef446ce6ffaa2bac220a09f44efd3d6c7557803b
eyawson <ebow.yawson@gmail.com>
2018-05-21 20:55:30 -0400

    add all variables

---
+++ js/game-logic.js
@@ -0,0 +1,14 @@
+// All code should be written in this file.
+let playerOneMoveOneType;
+let playerOneMoveTwoType;
+let playerOneMoveThreeType;
+let playerTwoMoveOneType;
+let playerTwoMoveTwoType;
+let playerTwoMoveThreeType;
+let playerOneMoveOneValue;
+let playerOneMoveTwoValue;
+let playerOneMoveThreeValue;
+let playerTwoMoveOneValue;
+let playerTwoMoveTwoValue;
+let playerTwoMoveThreeValue;
+
